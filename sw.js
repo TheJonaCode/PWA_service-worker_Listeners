5be4734fb2de3751b53519f23430a2a62af551ab
TheJonaCode <mcjhona069@gmail.com>
2022-10-01 18:48:44 -0500

    service-worker: activate

--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
 // Ciclo de vida del SW
 
 self.addEventListener('install', event => {
-    console.log('instalando sw');
+    console.log('instalando sw :)');
+
+});
+
+//Cuando el SW toma el control de la app
+self.addEventListener('activate', event => {
+    console.log(':) activo y listo');
 });
