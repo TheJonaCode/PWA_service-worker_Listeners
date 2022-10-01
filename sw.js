da38eb96dcbb5962a22c52cc27bf20cfd9619124
TheJonaCode <mcjhona069@gmail.com>
2022-10-01 18:50:55 -0500

    event.waitUntil()

--- sw.js
+++ sw.js
@@ -3,6 +3,17 @@
 self.addEventListener('install', event => {
     console.log('instalando sw :)');
 
+    const instalacion = new Promise((resulve, reject) => {
+        setTimeout(() => {
+            console.log('SW: Instalaciones terminadas');
+            self.skipWaiting();
+            resolve();
+        }, 1000);
+
+    });
+
+    event.waitUntil(instalacion);
+
 });
 
 //Cuando el SW toma el control de la app
