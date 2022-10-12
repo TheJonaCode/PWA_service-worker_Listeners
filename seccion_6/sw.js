1fa62891cb0a929f6c34a8376f2660a7ce7e9aca
TheJonaCode <mcjhona069@gmail.com>
2022-10-12 11:05:26 -0500

    Cache dinámico y optimizaciones

--- seccion_6/sw.js
+++ seccion_6/sw.js
@@ -1,15 +1,21 @@
+//const CACHE_NAME = 'cache-1';
+const CACHE_STATIC_NAME = 'static-v1';
+const CACHE_DYNAMIC_NAME = 'dynamic-v1';
+const CACHE_INMUTABLE_NAME = 'inmutable-v1';
+
 self.addEventListener('install', e => {
-    const cacheProm = caches.open('CACHE_NAME').then(cache => {
+    const cacheProm = caches.open(CACHE_STATIC_NAME).then(cache => {
         return cache.addAll([
             '/',
             '/index.html',
             '/css/style.css',
             'img/main.jpg',
-            'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
             '/js/app.js'
         ]);
     });
-    e.waitUntil(cacheProm);
+    const cacheInmutable = caches.open(CACHE_INMUTABLE_NAME)
+        .then(cache => cache.add('https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css'));
+    e.waitUntil(Promise.all([cacheProm, cacheInmutable]));
 });
 
 self.addEventListener('fetch', e => {
@@ -23,7 +29,7 @@ self.addEventListener('fetch', e => {
             //No existe archivo
             console.log('No existe', e.request.url);
             return fetch(e.request).then(newResp => {
-                caches.open(CACHE_NAME)
+                caches.open(CACHE_DYNAMIC_NAME)
                     .then(cache => {
                         cache.put(e.request, newResp);
                     });
