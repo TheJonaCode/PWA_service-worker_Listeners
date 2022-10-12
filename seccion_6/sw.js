efc228ca9da2329285529eb4ddacf139548a3a59
TheJonaCode <mcjhona069@gmail.com>
2022-10-12 11:20:16 -0500

    Cache y Network Race

--- seccion_6/sw.js
+++ seccion_6/sw.js
@@ -24,7 +24,8 @@ self.addEventListener('install', e => {
             '/index.html',
             '/css/style.css',
             'img/main.jpg',
-            '/js/app.js'
+            '/js/app.js',
+            '/img/no-image.jpg',
         ]);
     });
     const cacheInmutable = caches.open(CACHE_INMUTABLE_NAME)
@@ -33,6 +34,29 @@ self.addEventListener('install', e => {
 });
 
 self.addEventListener('fetch', e => {
+    //Cache & Network Race
+    const respuesta = new Promise((resolve, reject) => {
+        let rechazada = false;
+        const falloUnavez = () => {
+            if (rechazada) {
+                if (/\.(png|jpg)$/i.test(e.request.url)) {
+                    resolve(caches.match('/img/no-image.jpg'));
+                } else {
+                    reject('no se encontro respuesta')
+                }
+            } else {
+                rechazada = true;
+            }
+        }
+        fetch(e.request).then(res => {
+            res.ok ? resolve(res) : falloUnavez();
+        }).catch(falloUnavez);
+        caches.match(e.request).then(res => {
+            res ? resolve(res) : falloUnavez();
+        }).catch(falloUnavez);
+    });
+
+    e.repondWith(respuesta);
     //Cache Only
     //e.respondWith(caches.match(e.request));
 
@@ -70,7 +94,7 @@ self.addEventListener('fetch', e => {
     e.respondWith(respuesta);*/
 
     //Cache with network update
-    if (e.request.url.includes('bootstrap')) {
+    /*if (e.request.url.includes('bootstrap')) {
         return respondWith(caches.match(e.request));
     }
     const respuesta = caches.open(CACHE_STATIC_NAME).then(cache => {
@@ -79,6 +103,6 @@ self.addEventListener('fetch', e => {
         return cache.match(e.request);
     })
 
-    e.respondWith(respuesta);
+    e.respondWith(respuesta);*/
 
 });
